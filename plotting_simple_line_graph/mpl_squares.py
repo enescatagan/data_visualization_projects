2a075872e5c97544632d0536b75bc27ab869b4d0
enescatagan <enescatagan@gmail.com>
2023-02-12 00:47:38 +0300

    Change Label and Line Thickness

--- plotting_simple_line_graph/mpl_squares.py
+++ plotting_simple_line_graph/mpl_squares.py
@@ -4,6 +4,14 @@ squares = [1, 4, 9, 16, 25]
 
 # Generate one or more plots in the same figure
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()

40145efb8e3457bca3c432375ae1fb3843b51c3b
enescatagan <enescatagan@gmail.com>
2023-02-12 01:44:51 +0300

    Get Square Data Automatically

--- plotting_simple_line_graph/scatter_squares.py
+++ plotting_simple_line_graph/scatter_squares.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 # Style, Create subplot and scatter points
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,5 +17,7 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
+# Set the range for each axis.
+ax.axis([0, 1_100, 0, 1_100_000])
 
 plt.show()

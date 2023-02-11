01624187f34601503c007c2be65dc10cfca086dd
enescatagan <enescatagan@gmail.com>
2023-02-12 01:25:53 +0300

    Scatter Single Dot

--- plotting_simple_line_graph/scatter_squares.py
+++ plotting_simple_line_graph/scatter_squares.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
 
 # Give a style
 plt.style.use('seaborn')
 
 # Create a figure and a set of subplots.
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+
+# Plot single point with (x, y) values
+ax.scatter(2, 4, s=200)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -16,6 +16,7 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', which='major', labelsize=14)
+
 
 plt.show()

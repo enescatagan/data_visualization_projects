13aa346e74031be072439a044861620581c4e4ba
enescatagan <enescatagan@gmail.com>
2023-02-14 16:04:56 +0300

    Headers And their Positions

--- the_csv_file_format/sitka_highs.py
+++ the_csv_file_format/sitka_highs.py
@@ -5,4 +5,7 @@ filename = 'the_csv_file_format/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    # Show Header Row and index that belong to
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)

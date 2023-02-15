608919de89615b14220a9f5ba84b31be1a0cb960
enescatagan <enescatagan@gmail.com>
2023-02-15 16:01:10 +0300

    Extract Magnitude Values

--- mapping_global_data_sets/eq_explore_data.py
+++ mapping_global_data_sets/eq_explore_data.py
@@ -5,5 +5,15 @@ filename = 'mapping_global_data_sets/data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
+# Get All Earthquake data
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+
+mags = []
+# Get All Earthquake magnitudes
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+# Print first 10 Magnitude
+print(mags[:10])

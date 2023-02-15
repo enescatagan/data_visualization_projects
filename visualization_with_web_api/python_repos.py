aa950045c5fc3e4a792a37ed8d7807a68109e216
enescatagan <enescatagan@gmail.com>
2023-02-15 20:40:32 +0300

    Get All repos from API

--- visualization_with_web_api/python_repos.py
+++ visualization_with_web_api/python_repos.py
@@ -15,14 +15,13 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Examine the first Repository.
-repo_dict = repo_dicts[0]
-
 print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Respository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+for index, repo_dict in enumerate(repo_dicts):
+    print("\nRepo", index + 1, "\n----------")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Respository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")

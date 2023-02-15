d525751cb5c511de252c8cbb709f10f659b2a28e
enescatagan <enescatagan@gmail.com>
2023-02-15 16:11:09 +0300

    Extract Location Data

--- mapping_global_data_sets/eq_explore_data.py
+++ mapping_global_data_sets/eq_explore_data.py
@@ -1,5 +1,6 @@
 import json
 
+
 # Explore the structure of the data.
 filename = 'mapping_global_data_sets/data/eq_data_1_day_m1.json'
 with open(filename) as f:
@@ -8,12 +9,20 @@ with open(filename) as f:
 # Get All Earthquake data
 all_eq_dicts = all_eq_data['features']
 
-
-mags = []
-# Get All Earthquake magnitudes
+mags, lons, lats = [], [], []
+# Get All Earthquake magnitudes, and
+# Location Data: longitude and latitude 
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
 # Print first 10 Magnitude
 print(mags[:10])
+# Print first 5 longitude
+print(lons[:5])
+# Print first 5 latitude
+print(lats[:5])

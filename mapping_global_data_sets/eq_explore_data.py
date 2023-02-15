b98639fb8f6c1eef3c6201773d2cbd4389cd0711
enescatagan <enescatagan@gmail.com>
2023-02-15 16:24:52 +0300

    Build simple Map

--- mapping_global_data_sets/eq_explore_data.py
+++ mapping_global_data_sets/eq_explore_data.py
@@ -1,5 +1,8 @@
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 
 # Explore the structure of the data.
 filename = 'mapping_global_data_sets/data/eq_data_1_day_m1.json'
@@ -11,7 +14,7 @@ all_eq_dicts = all_eq_data['features']
 
 mags, lons, lats = [], [], []
 # Get All Earthquake magnitudes, and
-# Location Data: longitude and latitude 
+# Location Data: longitude and latitude
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
@@ -20,9 +23,9 @@ for eq_dict in all_eq_dicts:
     lons.append(lon)
     lats.append(lat)
 
-# Print first 10 Magnitude
-print(mags[:10])
-# Print first 5 longitude
-print(lons[:5])
-# Print first 5 latitude
-print(lats[:5])
+# Map the earthquakes.
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='mapping_global_data_sets/global_earthquakes.html')
